78b7acc78dcfac6cf9d04bb131240de2c84f08fe
Martin Hoecker-Martinez <mhoecker@coas.oregonstate.edu>
2016-04-15 10:16:34 -0400

    add descriptive comments

--- regridcores.m
+++ regridcores.m
@@ -22,21 +22,33 @@ glat	=	-90:dlat:(90-dlat);
 glon	=	-180:dlon:(180-dlon);
 Nlon	=	length(glon);
 Nlat	=	length(glat);
+% Gridded data
 datgrid	=	zeros(Nlon,Nlat);
+% How many data points are in that grid point
 ingrid	=	zeros(Nlon,Nlat);
+% indecies of the  grid cell is a particular data point in
 gridcell=	zeros(2,Ndata);
+% list of which data points are in a particular grid point
 incell	=	cell(Nlon,Nlat);
-% take average of values in each grid
+% Put each data point into a grid point
 for k=1:Ndata
+	%Find the nearest longitude
 	i		=	round((lon(k)-glon(1))/dlon);
 	i		=	mod(i,Nlon)+1;
+	%Find the nearest latitude
 	j		=	round((lat(k)-glat(1))/dlat);
 	j		=	mod(j,Nlat)+1;
+	% add the data index to that cells listing
 	incell{i,j}	=	[incell{i,j},k];
+	% add the grid cell index to that data points listing
 	gridcell(:,k)	=	[i,j];
+	% increase the count of data points in that grid cell
 	ingrid(i,j)	=	ingrid(i,j)+1;
+	% add the value to that cells running average
 	datgrid(i,j)	=	datgrid(i,j)+dat(k);
 end%for
+% take the average
+% Note: this will give NaNs for unoccupied cells
 datgrid	=	datgrid./ingrid;
 subplot(2,2,2)
 pcolor(glon,glat,datgrid')

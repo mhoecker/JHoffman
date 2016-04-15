3b57c65a26753eff2c49e8e7956cfc218b6711fa
Martin Hoecker-Martinez <mhoecker@coas.oregonstate.edu>
2016-04-15 10:09:13 -0400

    minor tweaks

--- regridcores.m
+++ regridcores.m
@@ -1,5 +1,5 @@
 %Set number of random data points
-Ndata	=	8192;
+Ndata	=	1024;
 %make random locations for "cores"
 xyz	=	2*rand(3,Ndata)-1	;
 xyz	=	xyz./sqrt(sum(xyz.*xyz,1));
@@ -8,16 +8,16 @@ lat	=	(180/pi)*acos(1-2*rand(1,Ndata))-90;
 %Core location longitude
 lon	=	360*rand(1,Ndata);
 %Value at eat sample location
-dat	=	sin(2*pi*lat/180).*cos(2*pi*lon/180)+.2*rand(1,Ndata);
+dat	=	sin(4*pi*lat/180).*cos(4*pi*lon/180)+.2*rand(1,Ndata);
 %Plot the location of the points
 subplot(2,2,1)
 plot(lon,lat,"o;;")
-xlim([-360,360])
+xlim([0,360])
 ylim([-90,90])
 title("Sample locations")
 % Make grid to put averages in
-dlat	=	3;
-dlon	=	5;
+dlat	=	9;
+dlon	=	10;
 glat	=	-90:dlat:(90-dlat);
 glon	=	-180:dlon:(180-dlon);
 Nlon	=	length(glon);
@@ -48,3 +48,5 @@ title("# of data points in cell")
 shading flat
 subplot(2,2,4)
 plot(1:Ndata,glat(gridcell(2,:)),"x;lat;",1:Ndata,glon(gridcell(1,:)),"+;lon;")
+xlim([1,Ndata])
+
